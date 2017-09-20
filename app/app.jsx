a128a9b72cc7cdf08120a8e4b657c3de4a042726
QuocMinh <cqminh1995@gmail.com>
2017-09-20 13:50:14 +0700

    First commit

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+let Hello = require('./comps/hello.jsx');
+
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
---
+++ app/comps/hello.jsx
@@ -0,0 +1,11 @@
+let React = require('react');
+
+let Hello = React.createClass({
+  render() {
+    return (
+      <h2>This is Hello component</h2>
+    );
+  }
+});
+
+module.exports = Hello;
